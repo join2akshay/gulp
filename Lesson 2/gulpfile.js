8894560a3d59c6331a0e56cdd77cdd48e77ca244
tvibhu12 <tvibhu12@gmail.com>
2019-02-01 15:26:06 +0530

    add gulp for scss and autoprefixer

--- Lesson 2/gulpfile.js
+++ Lesson 2/gulpfile.js
@@ -1,6 +1,20 @@
 const gulp = require("gulp");
-
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 gulp.task("default", function() {
   // code for your default task goes here
   console.log("hello world");
+});
+
+gulp.task("styles", function() {
+  gulp
+    .src("sass/**/*.scss")
+    .pipe(sass())
+    .on("error", sass.logError)
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"]
+      })
+    )
+    .pipe(gulp.dest("./css"));
 });
